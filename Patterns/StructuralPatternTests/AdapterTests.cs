b708466c909070d6a056669570075ea7e56f1f30
sidkcr <sidkcr@gmail.com>
2019-03-24 19:11:44 +0530

    Add adapter pattern example with unit tests

---
+++ Patterns/StructuralPatternTests/AdapterTests.cs
@@ -0,0 +1,23 @@
+
+namespace StructuralPatternTests
+{
+    using StructuralPatterns.Adapter;
+    using Xunit;
+
+    public class AdapterTests
+    {
+        [Fact]
+        public void ShouldReturnIWeatherFromGoogleWeather()
+        {
+            IWeather weather = new GoogleWeatherAdapter(new GoogleWeather());
+            Assert.Equal(32, weather.GetTemprature(411058));
+        }
+
+        [Fact]
+        public void ShouldReturnIWeatherFromAccuWeather()
+        {
+            IWeather weather = new AccuWeatherAdapter(new AccuWeather());
+            Assert.Equal(30, weather.GetTemprature(411058));
+        }
+    }
+}
