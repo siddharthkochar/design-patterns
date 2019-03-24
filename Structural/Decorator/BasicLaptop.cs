37e2db9156c835559db8a940c42aff7905f89013
Siddharth Kochar <sidkcr@gmail.com>
2019-03-24 12:14:10 -0700

    Add decorator pattern example with unit tests (#4)

---
+++ Structural/Decorator/BasicLaptop.cs
@@ -0,0 +1,13 @@
+﻿namespace StructuralPatterns.Decorator
+{
+    public class BasicLaptop : ILaptop
+    {
+        public int Cost => 30000;
+
+        public int HardDisk => 250;
+
+        public int Ram => 8;
+
+        public int Gpu => 0;
+    }
+}
