89c9f8ca658aaa3597dbafb1decf6785280d84c2
sidkcr <sidkcr@gmail.com>
2019-03-24 21:42:31 +0530

    Add facade pattern example with unit tests

---
+++ StructuralPatternTests/FacadeTests.cs
@@ -0,0 +1,18 @@
+﻿namespace StructuralPatternTests
+{
+    using StructuralPatterns.Facade;
+    using Xunit;
+
+    public class FacadeTests
+    {
+        [Fact]
+        public void ShouldNotifyUserByEmailAndMessage()
+        {
+            INotification notification = new Notification();
+            Assert.True(notification.Notify("hello", "user@gmail.com"));
+            Assert.False(notification.Notify(null, "user@gmail.com"));
+            Assert.False(notification.Notify(null, null));
+            Assert.False(notification.Notify("hello", null));
+        }
+    }
+}
