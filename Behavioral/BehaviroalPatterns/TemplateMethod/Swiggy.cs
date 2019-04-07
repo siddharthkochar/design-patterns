77ab7f7a072da1b2d09be1e4af78797bc3dc0936
Siddharth Kochar <sidkcr@gmail.com>
2019-04-07 01:51:52 -0700

    Add template method design pattern sample (#6)

---
+++ Behavioral/BehaviroalPatterns/TemplateMethod/Swiggy.cs
@@ -0,0 +1,17 @@
+﻿namespace BehavioralPatterns.TemplateMethod
+{
+    using System;
+
+    public class Swiggy : FoodDelivery
+    {
+        protected override void TakeOrder() => throw new NotImplementedException();
+
+        protected override void ProcessPayment() => throw new NotImplementedException();
+
+        protected override void Prepare() => throw new NotImplementedException();
+
+        protected override void Cook() => throw new NotImplementedException();
+
+        protected override void Deliver() => throw new NotImplementedException();
+    }
+}
