d5897f786ebdb3eeb6c9a5864c81d81acb2e517c
sidkcr <sidkcr@gmail.com>
2019-04-07 14:18:36 +0530

    Add template method design pattern sample

---
+++ Behavioral/BehaviroalPatterns/TemplateMethod/FoodDelivery.cs
@@ -0,0 +1,19 @@
+﻿namespace BehavioralPatterns.TemplateMethod
+{
+    public abstract class FoodDelivery
+    {
+        protected abstract void TakeOrder();
+        protected abstract void ProcessPayment();
+        protected abstract void Prepare();
+        protected abstract void Cook();
+        protected abstract void Deliver();
+        public void ProcessOrder()
+        {
+            TakeOrder();
+            ProcessPayment();
+            Prepare();
+            Cook();
+            Deliver();
+        }
+    }
+}
