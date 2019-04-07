47818e9d385f3e54cdc2e0fc1152e106db66a9e1
sidkcr <sidkcr@gmail.com>
2019-04-07 10:40:18 +0530

    Add strategy pattern

---
+++ Behavioral/BehaviroalPatterns/Strategy/Strategy.cs
@@ -0,0 +1,36 @@
+﻿namespace BehavioralPatterns.Strategy
+{
+    using System.ComponentModel;
+
+    public class Strategy
+    {
+        public enum LoggingConfiguration
+        {
+            File = 0,
+            Database = 1,
+            ApplicationInsights = 2
+        };
+
+        private readonly ILogger logger;
+
+        public Strategy(LoggingConfiguration config)
+        {
+            switch (config)
+            {
+                case LoggingConfiguration.File:
+                    logger = new FileLogger();
+                    break;
+                case LoggingConfiguration.Database:
+                    logger = new DatabaseLogger();
+                    break;
+                case LoggingConfiguration.ApplicationInsights:
+                    logger = new ApplicationInsightsLogger();
+                    break;
+                default:
+                    throw new InvalidEnumArgumentException();
+            }
+        }
+
+        public string LogMessage(string message) => logger.Log(message);
+    }
+}
